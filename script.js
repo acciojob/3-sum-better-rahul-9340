d06e73b9405a70ea256ae99ee5c692f507f11bb0
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-04 20:46:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function threeSum(arr, target)  {
+	let closestsum  = Infinity;
+	let finalsum;
+  for (let i = 0; i < arr.length - 2; i++) {
+    let sum = 0;
+    for (let j = i; j <= i + 2; j++) {
+      sum += arr[j];
+    }
+    if (sum === target) {
+      return sum;
+    }
 
+   let currentdif = Math.abs(sum-target);
+	  
+    if (currentdif<closestsum) {
+	     closestsum = currentdif;
+		finalsum = sum;
+     }
+	  
+      }
+	  return finalsum
+                    
+  }
 module.exports = threeSum;
+
+  
+
+// let arr = [1, 2, 4, 3];
+// console.log(threeSum(arr, 5));
