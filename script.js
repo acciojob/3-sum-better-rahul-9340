5ddf2a0a4b5af7e29356cea4c407a4a026e2d703
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-05 13:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-function threeSum(arr, target)  {
-	let closestsum  = Infinity;
-	let finalsum;
-  for (let i = 0; i < arr.length - 2; i++) {
-    let sum = 0;
-    for (let j = i; j <= i + 2; j++) {
-      sum += arr[j];
-    }
-    if (sum === target) {
-      return sum;
-    }
+function threeSum(arr,target) {
+  const n = arr.length;
+  let closestdiff = Infinity;
+  let closestsum;
 
-   let currentdif = Math.abs(sum-target);
-	  
-    if (currentdif<closestsum) {
-	     closestsum = currentdif;
-		finalsum = sum;
-     }
-	  
+  for (let i = 0; i < n - 2; i++) {
+    let left = i + 1;
+    let right = n - 1;
+
+    while (left < right) {
+      let sum = arr[i] + arr[left] + arr[right];
+      let currentdiff = Math.abs(sum - target);
+
+      if (currentdiff < closestdiff) {
+        closestdiff = currentdiff;
+        closestsum = sum;
       }
-	  return finalsum 
+
+      if (sum === target) {
+        return target; // Return the target sum instantly when found
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
   }
 
-module.exports = threeSum;
+  return closestsum; // Return the closest sum when target sum is not found
+}
 
-  
+let arr = [1, 2, 5, 2, 2];
+console.log(threeSum(arr, 5)); // Returns 5 because it found the target sum
 
-// let arr = [1, 2, 4, 3];
-// console.log(threeSum(arr, 5));
